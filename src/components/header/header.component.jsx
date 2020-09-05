5d47be5583cc5128a7a8ac554857fae6b8e00830
Lima <limatembe44@gmail.com>
2020-09-05 20:52:26 +0200

    integrated firebase to our store, added the sign in with google component and sign out

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+import { auth } from "../../firebase/firebase.utils";
+
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import "./header.styles.scss";
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo />
@@ -15,6 +18,13 @@ const Header = () => (
       <Link className="option" to="/contact">
         CONTACT
       </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link to="/signin">SIGN IN</Link>
+      )}
     </div>
   </div>
 );
